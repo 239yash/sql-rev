c0175427897282dae2b6c52301f9e02177e76e80
Yash Gupta <53759576+239yash@users.noreply.github.com>
2023-07-24 08:11:20 +0530

    Create 1517-Find Users With Valid E-Mails.sql

---
+++ 1517-Find Users With Valid E-Mails.sql
@@ -0,0 +1,28 @@
+SELECT * FROM users WHERE 
+        mail REGEXP '^[a-zA-Z][a-zA-Z-._0-9]*@leetcode[.]{1}com$';
+
+
+
+
+
+
+
+
+
+
+
+
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- | mail          | varchar |
+-- +---------------+---------+
+-- In SQL, user_id is the primary key for this table.
+-- This table contains information of the users signed up in a website. Some e-mails are invalid. Find the users who have valid emails. A valid e-mail has a prefix name and a domain where:
+
+-- The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+-- The domain is '@leetcode.com'.
