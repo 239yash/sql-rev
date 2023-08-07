a3aea718e2fe8f3934716641906568f63dc0abd1
Yash Gupta <53759576+239yash@users.noreply.github.com>
2023-08-07 08:22:01 +0530

    Create 2082-The Number of Rich Customers.sql

---
+++ 2082-The Number of Rich Customers.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+SELECT COUNT(DISTINCT(customer_id)) as rich_count 
+FROM Store
+WHERE amount > 500;
+
+
+
+
+
+
+
+
+
+
+-- Table: Store
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | bill_id     | int  |
+-- | customer_id | int  |
+-- | amount      | int  |
+-- +-------------+------+
+-- bill_id is the primary key (column with unique values) for this table.
+-- Each row contains information about the amount of one bill and the customer associated with it.
+ 
+
+-- Write a solution to report the number of customers who had at least one bill with an amount strictly greater than 500.
