c48232f69758b7af004df017f5422a8d00022ad6
Yash Gupta <53759576+239yash@users.noreply.github.com>
2023-08-23 08:41:18 +0530

    Update 184-Department Highest Salary.sql

--- 184-Department Highest Salary.sql
+++ 184-Department Highest Salary.sql
@@ -1,7 +1,15 @@
-SELECT Department.name AS Department ,Employee.name AS Employee, Employee.salary FROM Department  
-JOIN Employee 
-ON Employee.departmentId = Department.id 
-WHERE(departmentId, salary) IN (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId) ;
+# SELECT Department.name AS Department ,Employee.name AS Employee, Employee.salary FROM Department  
+# JOIN Employee 
+# ON Employee.departmentId = Department.id 
+# WHERE(departmentId, salary) IN (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId) ;
+
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId);
+
 
 
 
